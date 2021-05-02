4d83940aaca3b0da6b6b062692abe0eb26789943
Teddy Kekana <teddykekana@Teddys-MacBook-Air.local>
2021-05-02 03:38:53 +0200

    fix(test): Test Lambda end-point

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@
 import {Layout} from './components/layout/BaseLayout';
 import { useSelector } from 'react-redux';
 import {ViewPeople} from './components/ViewPeople';
-
+import ApiConfig from "./config/ApiConfig";
 const App = (props: any) => {
     const { page } = useSelector((state: any) => state.paginationReducer);
+    console.log(`${ApiConfig}`);
+    console.log(`${page}`);
     return (
         <Layout>
             <ViewPeople page={page} props={props}  />
